450106c39a0f4ed866cdb9e9dc7e7d3672290681
Saad Coder <25401728+saadjatt@users.noreply.github.com>
2018-10-16 01:50:03 +0500

    Done with app

---
+++ App.js
@@ -0,0 +1,38 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reloadsssssssss,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View style={styles.container}>
+       
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+   
+  },
+  
+});
---
+++ app/components/Main.js
@@ -0,0 +1,36 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  TextInput,
+  ScrollView,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+
+
+type Props = {};
+export default class Main extends Component<Props> {
+  render() {
+    return (
+     <View>
+     <Text>Testing</Text>
+     </View> 
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+   
+  },
+  
+});
